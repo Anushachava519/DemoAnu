655fbfcc5a011396077a2f291834c66ce729c395
Anusha <anushachava519@gmail.com>
2022-02-14 14:57:44 -0500

    Updated MyfirstCommit

---
+++ ArithOpera/src/ArithOpera.java
@@ -0,0 +1,14 @@
+
+public class ArithOpera {
+  public static void main(String[] args) {
+	  int a=10;
+	  int b=20;
+	  int sub=a-b;
+	  System.out.println("The result of addition is "+ a+b);
+	  System.out.println("The result of subtraction is "+ sub);
+	  System.out.println("The result of multiplication is "+ a*b);
+	  System.out.println("The result of division is "+ a/b);
+	  System.out.println("The result of modulus is "+ a%b);
+	  
+  }
+}
